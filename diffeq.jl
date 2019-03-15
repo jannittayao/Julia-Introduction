13812ab89a01da32f449b7efb3aac46f636aaf67
Jannitta Yao <jyao@wellesley.edu>
2019-03-15 10:59:02 -0400

    updated diffeq.jl with manual, analytical, and DifferentialEquations soln

--- diffeq.jl
+++ diffeq.jl
@@ -1,10 +1,33 @@
 using DifferentialEquations, Plots
+gr()
 
-f(u, p, t) = (cos(2*pi*t)+randn(1)[1])
+# manually calculate diffeq
+time = collect(0:0.1:10)
+solns_manual = Float64[]
+
+
+for t in time
+    t = ((sin(2*pi*t)/(2*pi)) + (randn(1)[1] * 0.1))
+    push!(solns_manual, t)
+end
+
+# using DifferentialEquations package to calculate diffeq
+f(u, p, t) = cos(2*pi*t)
 u0 = 0.0
 tspan = (0.0, 10.0)
 prob = ODEProblem(f, u0, tspan)
-soln = solve(prob)
+soln_diffeq = solve(prob)
 
-plot(soln,linewidth=1,title="Solution to the ODE",
-     xaxis="Time (s)",yaxis="X(t)") # legend=false
+# Calculating diffeq analytically
+solns_analytical = Float64[]
+
+for t in time
+    t = ((sin(2*pi*t)/(2*pi)))
+    push!(solns_analytical, t)
+end
+
+
+plot(soln_diffeq,linewidth=1,title="Solution to the ODE",
+     xaxis="Time (s)",yaxis="X(t)")
+plot!(time, solns_manual, linewidth = 1)
+plot!(time, solns_analytical, linewidth = 1)
