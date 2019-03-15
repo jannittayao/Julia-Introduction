bcb369b0e1c570cd0e0833d1b35d5ad06c6aa6cd
Jannitta Yao <jyao@wellesley.edu>
2019-03-15 11:37:05 -0400

    Update diffeq.jl

--- diffeq.jl
+++ diffeq.jl
@@ -7,7 +7,7 @@ solns_manual = Float64[]
 
 
 for t in time
-    t = ((sin(2*pi*t)/(2*pi)) + (randn(1)[1] * 0.1))
+    t = ((sin(2*pi*t)/(2*pi)) + (randn(1)[1] * 0.1*time))
     push!(solns_manual, t)
 end
 
