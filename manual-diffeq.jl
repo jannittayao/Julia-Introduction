1b39372106b3da3c74fb6600ea01b7d37afa3623
Jannitta Yao <jyao@wellesley.edu>
2019-03-20 10:39:18 -0400

    Update manual-diffeq.jl

--- manual-diffeq.jl
+++ manual-diffeq.jl
@@ -2,14 +2,24 @@ using Plots
 gr()
 
 time = collect(0:0.1:10)
-solns = Float64[]
+xArray = Float64[]
 
-
-for t in time
-    t = ((sin(2*pi*t)/(2*pi)) + (randn(1)[1] * t))
-    push!(solns, t)
+function f(t)
+    f = cos(2.0*pi*t) + 0.1*rand(1)[1];
 end
 
+#f(t) values
+for t in time
+    push!(xArray, f(t))
+end
+
+#Euler method
+solns = zeros(size(time)[1]);
+for i = 2:size(time)[1]
+    solns[i] = solns[i-1] + 0.1 * xArray[i-1]
+end
+
+#plot
 plot(time, solns)
 xlabel!("Time (s)")
 ylabel!("X(t)")
