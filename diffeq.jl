f4134ad4e22ddeab94cb7d6b89aa5ea125389e19
Jannitta Yao <jyao@wellesley.edu>
2019-04-02 23:02:26 -0400

    updated manual-diffeq.jl; abstracted over manual solution

--- diffeq.jl
+++ diffeq.jl
@@ -1,15 +1,47 @@
 using DifferentialEquations, Plots
 gr()
 
-# manually calculate diffeq
-time = collect(0:0.1:10)
-solns_manual = Float64[]
-
-
-for t in time
-    t = ((sin(2*pi*t)/(2*pi)) + (randn(1)[1] * 0.1 * t))
-    push!(solns_manual, t)
+function f(t)
+    f = cos(2.0*pi*t) + 0.1*rand(1)[1];
 end
+
+# manually calculate diffeq
+function manual_plot(min, max, step)
+    #timestep
+    delta_t = step
+    max_t = max
+    t = min
+    nstep_t = max_t/delta_t
+    x_init = 0
+
+    #create time step array
+    time = collect(t:delta_t:max_t)
+    #empty array for X'(t) values
+    drvArray = Float64[]
+    #empty array for X(t) values
+    xArray = Float64[]
+    push!(xArray, 0) #adds X(0) value to xArray
+
+
+    #Euler method (efficient)
+    let inner_t = t, x = x_init # let deals with for loop scope in julia
+        for n = 1:nstep_t
+            #increment t by 0.1
+            inner_t += delta_t
+
+            #find X'(t) and add it to drvArray
+            drv = f(inner_t)
+            push!(drvArray,drv)
+
+            #find X(t) and add it to xArray
+            x += drv * delta_t
+            push!(xArray, x)
+        end
+    end
+    manualPlotArray = [time, xArray]
+end
+
+man_plot = manual_plot(0.0, 10, 0.1)
 
 # using DifferentialEquations package to calculate diffeq
 f(u, p, t) = cos(2*pi*t)
@@ -29,5 +61,5 @@ end
 
 plot(soln_diffeq,linewidth=1,title="Solution to the ODE",
      xaxis="Time (s)",yaxis="X(t)", label="diffeq")
-plot!(time, solns_manual, linewidth = 1, label="manual soln")
+plot!(man_plot[1], man_plot[2], linewidth = 1, label="manual soln")
 plot!(time, solns_analytical, linewidth = 1, label = "analytical soln")
--- manual-diffeq.jl
+++ manual-diffeq.jl
@@ -5,38 +5,39 @@ function f(t)
     f = cos(2.0*pi*t) + 0.1*rand(1)[1];
 end
 
-#timestep
-delta_t = 0.1
-max_t = 10
-t = 0.0
-nstep_t = max_t/delta_t
-x_init = 0
+function manual_plot(min, max, step)
+    #timestep
+    delta_t = step
+    max_t = max
+    t = min
+    nstep_t = max_t/delta_t
+    x_init = 0
 
-#create time step array
-time = collect(init_t:delta_t:max_t)
-#empty array for X'(t) values
-drvArray = Float64[]
-#empty array for X(t) values
-xArray = Float64[]
-push!(xArray, 0) #adds X(0) value to xArray
+    #create time step array
+    time = collect(t:delta_t:max_t)
+    #empty array for X(t) values
+    xArray = Float64[]
+    push!(xArray, 0) #adds X(0) value to xArray
 
 
-#Euler method (efficient)
-let inner_t = t, x = x_init # let deals with for loop scope in julia
-    for n = 1:nstep_t
-        #increment t by 0.1
-        inner_t += delta_t
+    #Euler method (efficient)
+    # let deals with for loop scope in julia
+    let inner_t = t, x = x_init, drv = f(0)
+        for n = 1:nstep_t
+            #increment t by 0.1
+            inner_t += delta_t
 
-        #find X'(t) and add it to drvArray
-        drv = f(inner_t)
-        push!(drvArray,drv)
+            #find X(t) and add it to xArray
+            x += drv * delta_t
+            push!(xArray, x)
 
-        #find X(t) and add it to xArray
-        x += drv
-        push!(xArray, x)
+            #update X'(t) with new t
+            drv = f(inner_t)
+
+        end
     end
+    manualPlotArray = [time, xArray]
 end
-
 
 #Euler method (original + less efficient)
 # solns = zeros(size(time)[1]);
@@ -45,6 +46,7 @@ end
 # end
 
 #plot
-plot(time, xArray)
+testplot = manual_plot(0, 10, 0.1)
+plot(testplot[1], testplot[2])
 xlabel!("Time (s)")
 ylabel!("X(t)")
